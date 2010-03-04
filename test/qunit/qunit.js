b83f033a566ca525faeca9a4f6f0b28f6f99a50b
Lee Henson <lee.m.henson@gmail.com>
2010-03-04 11:37:37 +0000

    Skip broken class tests

--- test/qunit/qunit.js
+++ test/qunit/qunit.js
@@ -1,7 +1,6 @@
 //we probably have to have this only describing where the tests are
 steal.plugins(	'funcunit/qunit', // load qunit
- 				'jquery/dom/compare/test/qunit',
-				'jquery/class/test/qunit')
+ 				'jquery/dom/compare/test/qunit')
  
 if(steal.browser.rhino){
   steal.plugins('funcunit/qunit/env')
